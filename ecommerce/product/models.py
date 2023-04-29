701121eef7394c1245ad511645ca6a25723e3b1b
lazy-billy <dizzy95@list.ru>
2023-04-29 23:31:49 +0500

    added serializers and views for products

--- ecommerce/product/models.py
+++ ecommerce/product/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 
 
-class Category(models.Model):
+class Category(MPTTModel):
     name = models.CharField(max_length=100)
     parent = TreeForeignKey('self', on_delete=models.PROTECT, null=True, blank=True)
     
